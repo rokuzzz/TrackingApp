2da8ca4b0842b99b7f79456cbdbcca5f4878b4c6
Roman Kuzero <rombeldan@gmail.com>
2021-10-14 22:37:01 +0300

    Total run statistics display in the statistics fragment. Functionality to change the username and weight implemented.

--- app/src/main/java/com/example/trackingapp/ui/fragments/StatisticsFragment.kt
+++ app/src/main/java/com/example/trackingapp/ui/fragments/StatisticsFragment.kt
@@ -1,13 +1,54 @@
 package com.example.trackingapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.example.trackingapp.R
+import com.example.trackingapp.other.TrackingUtility
 import com.example.trackingapp.ui.viewmodels.StatisticsViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_statistics.*
+import kotlin.math.round
 
 @AndroidEntryPoint
 class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
 
     private val viewModel: StatisticsViewModel by viewModels()
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        subscribeToObservers()
+    }
+    private fun subscribeToObservers() {
+        viewModel.totalTimeRun.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                val totalTimeRun = TrackingUtility.getFormattedStopWatchTime(it)
+                tvTotalTime.text = totalTimeRun
+            }
+        })
+        viewModel.totalDistance.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                val km = it / 1000f
+                val totalDistance = round(km * 10f) / 10f
+                val totalDistanceString = "${totalDistance}km"
+                tvTotalDistance.text = totalDistanceString
+            }
+        })
+        viewModel.totalAvgSpeed.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                val avgSpeed = round(it * 10f) / 10f
+                val avgSpeedString = "${avgSpeed}km/h"
+                tvAverageSpeed.text = avgSpeedString
+            }
+        })
+        viewModel.totalCaloriesBurned.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                val totalCalories = "${it}kcal"
+                tvTotalCalories.text = totalCalories
+            }
+        })
+    }
 }
